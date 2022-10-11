36f698ce3d1f53103562bd7d4c5557d30c208bfe
EunjinBang <twin1963@sookmyung.ac.kr>
2022-10-11 16:57:59 +0900

    03_01

--- main.c
+++ main.c
@@ -3,17 +3,22 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void func(void)
-{
-	int x;
-	printf("func x is at %p\n", &x);
-}
+int sumTwo();
 
 int main(int argc, char *argv[]) {
 	
-	int x;
-	printf("main x is at %p\n", &x);
-	func();
+	int x; 
+	int y;
+	int result;
+	printf("더하고 싶은 두 수를 입력하시오:");
+	scanf("%d %d", &x, &y);
+	result = sumTwo(x, y);
+	printf("%d", result);
 	
 	return 0;
+}
+
+int sumTwo(int a, int b)
+{
+	return (a + b);
 }

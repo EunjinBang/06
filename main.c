2c2d53d49ef1a05d2406d45f98d1922561c760c6
EunjinBang <twin1963@sookmyung.ac.kr>
2022-10-11 17:04:23 +0900

    04_01

--- main.c
+++ main.c
@@ -3,27 +3,18 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int get_max();
+void square(int a)
+{
+	a = a*a;
+ } 
 
 int main(int argc, char *argv[]) {
+	int a =2;
 	
-	int a;
-	int b; 
-	int maxValue;
-	printf("큰 수를 계산할 두 수를 입력하시오:");
-	scanf("%d %d", &a, &b);
-	maxValue = get_max(a, b);
-	printf("%d", maxValue);
+	square(a);
+	printf("a = %i\n",a);
 	
 	return 0;
 }
 
-int get_max(int x, int y)
-{
-	int output;
-	if (x > y)
-		output = x;
-	else
-		output = y;
-	return output;
-}
+

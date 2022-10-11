2160ef32a14dba8a7db55d8c67b1e542d00ec3ea
EunjinBang <twin1963@sookmyung.ac.kr>
2022-10-11 16:59:00 +0900

    03_02

--- main.c
+++ main.c
@@ -8,17 +8,16 @@ int sumTwo();
 int main(int argc, char *argv[]) {
 	
 	int x; 
-	int y;
 	int result;
-	printf("더하고 싶은 두 수를 입력하시오:");
-	scanf("%d %d", &x, &y);
-	result = sumTwo(x, y);
+	printf("제곱하고 싶은 수를 입력하시오:");
+	scanf("%d", &x);
+	result = square(x);
 	printf("%d", result);
 	
 	return 0;
 }
 
-int sumTwo(int a, int b)
+int square(int n)
 {
-	return (a + b);
+	return (n * n);
 }
